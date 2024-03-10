5cd05141d94a31516a80a03d06d1484d99c96198
aimeudoxie <eudoxieumwali@gmail.com>
2024-03-10 22:26:06 +0200

    cloudinary updates

--- src/helpers/cloudinary.ts
+++ src/helpers/cloudinary.ts
@@ -1,9 +1,14 @@
-import {v2 as cloudinary} from 'cloudinary';
-          
-cloudinary.config({ 
+// src/helpers/cloudinary.ts
+import { v2 as cloudinaryV2, ConfigOptions } from 'cloudinary';
+
+const cloudinary = cloudinaryV2 as any; // Override the type temporarily
+
+const cloudinaryConfig: ConfigOptions = { 
   cloud_name: 'dmhy5ah1b', 
   api_key: '199238467323671', 
   api_secret: 'zXAGW-h9KNpkhbl9eRRRjpHPQLo' 
-});
+};
+
+cloudinary.config(cloudinaryConfig);
 
 export default cloudinary;
--- src/middleware/uploadMiddleware.ts
+++ src/middleware/uploadMiddleware.ts
@@ -1,5 +1,8 @@
 import multer from "multer";
-import * as uuid from "uuid";
+// src/middleware/uploadMiddleware.ts
+import { v4 as uuidv4 } from 'uuid';
+// Rest of your code...
+
 import * as path from "path";
 
 type DestinationCallback = (error: Error | null, destination: string) => void;
@@ -7,7 +10,7 @@ type FileNameCallback = (error: Error | null, filename: string) => void;
 
 const diskStorage = multer.diskStorage({
     filename: (req, file, callback) => {
-        callback(null, uuid.v4() + path.extname(file.originalname));
+        callback(null, uuidv4() + path.extname(file.originalname));
     },
 });
 const limits = {
