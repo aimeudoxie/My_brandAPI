746c57c31d8583f26e967b6106bd3e23814884ec
aimeudoxie <eudoxieumwali@gmail.com>
2024-03-10 23:11:42 +0200

    cloudinary

--- src/controllers/articleController.ts
+++ src/controllers/articleController.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from 'express';
 import { Article, IArticle } from '../model/article';
 import { validateArticle,validateupdatedArticle } from '../validations/articleValidation'; 
-import cloudinary from '../helpers/cloudinary';
-import upload from '../middleware/uploadMiddleware';
+import { v2 as cloudinary} from "cloudinary";
+import 'dotenv/config';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
 
 class ArticleController {
   async createArticle(req: Request, res: Response): Promise<Response | IArticle> {
