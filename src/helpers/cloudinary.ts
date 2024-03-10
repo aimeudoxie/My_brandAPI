1de3bd37cd3ce5dc272b2131a8f4d8fcb776f453
aimeudoxie <eudoxieumwali@gmail.com>
2024-03-10 22:50:20 +0200

    cloudinary updates

--- src/helpers/cloudinary.ts
+++ src/helpers/cloudinary.ts
@@ -1,14 +1,9 @@
-// src/helpers/cloudinary.ts
-import { v2 as cloudinaryV2, ConfigOptions } from 'cloudinary';
-
-const cloudinary = cloudinaryV2 as any; // Override the type temporarily
-
-const cloudinaryConfig: ConfigOptions = { 
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
   cloud_name: 'dmhy5ah1b', 
   api_key: '199238467323671', 
   api_secret: 'zXAGW-h9KNpkhbl9eRRRjpHPQLo' 
-};
-
-cloudinary.config(cloudinaryConfig);
+});
 
 export default cloudinary;
